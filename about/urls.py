b8e46779d0bd40ea7e8a4f124524477b4c32784a
karchon <157898954+hossin13851385@users.noreply.github.com>
2026-09-05 23:27:22 +0330

    add blog app
    add blog app and add contact to about app

--- about/urls.py
+++ about/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path , include
 from . import views
+
+
+app_name = 'about'
 urlpatterns = [
-    path('' , views.about , name = 'about')
-]
+    path('' , views.about , name = 'about'),
+    path('cantact/' , views.contact , name = 'cantact')
+] 
--- about/views.py
+++ about/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def about(request):
     return render(request , 'about/about-us.html')
+
+def contact(request):
+    return render(request , 'about/contact-us.html')

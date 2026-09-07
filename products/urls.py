40c403d24c5c0743734fa51d7eb1862b3a40c02b
karchon <157898954+hossin13851385@users.noreply.github.com>
2026-09-07 23:33:58 +0330

    add single product
    add single product in project but dont match it

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'product'
 
 urlpatterns = [
-    
+    path('single_product' , views.single , name= 'single' ),
     path('category/' , views.category , name = 'category')
 ]
--- products/views.py
+++ products/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def category(request):
     return render (request , 'product/list-category.html')
+
+
+def single(request):
+    return render (request , 'product/single-product.html')

e6c5edc63bf432accb4266080068bac3c2aceaa1
karchon <157898954+hossin13851385@users.noreply.github.com>
2026-09-09 23:29:01 +0330

    add model product to products app

--- masay/settings.py
+++ masay/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'home', 
     'about', 
     'blog',
+    'products',
 
 ]
 
--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    name = models.CharField(max_length= 50)
+    descrip = models.TextField()
+    price = models.IntegerField()
+    picture = models.CharField(max_length=100)
+    creat_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now_add=True)
